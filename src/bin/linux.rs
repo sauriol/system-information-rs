0c79256df05b136761c73fef84836fac9fd5bae7
Jack Sauriol <saurioljohn@gmail.com>
2018-03-06 23:41:55 -0500

    Added (untested) detection for some Linux distros

--- src/bin/linux.rs
+++ src/bin/linux.rs
@@ -2,10 +2,11 @@ extern crate regex;
 
 use regex::Regex;
 use std::process::Command;
+use std::path::Path;
 
-struct Linux_Info {
-    distro: String,
-    version: String
+struct DistroInfo {
+    distro: Option<String>,
+    version: Option<String>,
 }
 
 pub fn get_username() -> Option<String> {
@@ -20,4 +21,37 @@ pub fn get_hostname() -> Option<String> {
         Ok(output) => Some(String::from_utf8(output.stdout).unwrap()),
         Err(_) => None
     }
+}
+
+pub fn get_distro_info() -> Option<DistroInfo> {
+
+}
+
+fn get_distro() -> Option<String> {
+    if Path::new("/etc/os-release").exists() {
+        return parse_os_release()
+    }
+    else if Path::new("/etc/arch-release").exists() {
+        return Some("Arch Linux".to_owned())
+    }
+    else if Path::new("/etc/gentoo-release").exists() {
+        return Some("Gentoo Linux".to_owned())
+    }
+    else if Path::new("/etc/fedora-release").exists() {
+        return Some("Fedora Linux".to_owned())
+    }
+    else if Path::new("/etc/redhat-release").exists() {
+        return Some("Red Hat Enterprise Linux".to_owned())
+    }
+    else if Path::new("/etc/debian_version").exists() {
+        return Some("Debian GNU/Linux".to_owned())
+    }
+    else {
+        return None
+    }
+}
+
+/// Parses the /etc/os-release file and returns the PRETTY_NAME
+fn parse_os_release() -> Option<String> {
+
 }
