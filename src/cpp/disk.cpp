b302adcd9e58e20d714eddbd8d92237ac0629ac3
Jack Sauriol <saurioljohn@gmail.com>
2018-03-18 16:49:38 -0400

    Added disk.cpp, to get info about the filesystem

---
+++ src/cpp/disk.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <boost/filesystem.hpp>
+
+using namespace boost::filesystem;
+using std::cout;
+using std::endl;
+
+int main(int argc, char* argv[])
+{
+    if (argc != 2) {
+        return 1;
+    }
+    space_info si = space(argv[1]);
+
+    uintmax_t in_use = si.capacity- si.free;
+
+    cout << si.capacity << endl;
+    cout << si.free << endl;
+    cout << in_use << endl;
+}
